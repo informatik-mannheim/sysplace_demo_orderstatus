26e9a8a555036beb17f8eeb06e83b6608b3995a2
GrabTheScreen <valentinaburjan@googlemail.com>
2017-03-20 15:21:06 +0100

    UI changes, new background

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -59,5 +59,15 @@ namespace TangibleTouch.Properties {
                 resourceCulture = value;
             }
         }
+        
+        /// <summary>
+        ///   Sucht eine lokalisierte Ressource vom Typ System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap car_production {
+            get {
+                object obj = ResourceManager.GetObject("car_production", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
